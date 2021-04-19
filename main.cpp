282e187f60e67d0fbb56a896427ca427055fc531
Jsam88 <60284648+Jsam88@users.noreply.github.com>
2021-04-18 18:48:14 -0700

    Revert "Jsam88/class files"

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
 
 #include "base.hpp"
-#include "Mult.hpp"
-#include "Div.hpp"
-#include "Random.hpp"
-#include "op.hpp"
-#include <string>
 
 int main() {
-    This is a very basic main, and being able to correctly execute this main
-    does not constitute a completed lab. Make sure you write unit tests for
-    all the classes that you create (and can be instantiated) in this lab
+    // This is a very basic main, and being able to correctly execute this main
+    // does not constitute a completed lab. Make sure you write unit tests for
+    // all the classes that you create (and can be instantiated) in this lab
     Base* three = new Op(3);
     Base* seven = new Op(7);
     Base* four = new Op(4);
