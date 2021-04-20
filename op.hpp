1543f247bd99bdc9f4d61e766139f74eb472141a
Jsam88 <jordansam@sbcglobal.net>
2021-04-20 12:05:53 -0700

    Added mock_test and edited op.hpp

--- op.hpp
+++ op.hpp
@@ -22,29 +22,4 @@ class Op : public Base {
 		}		
 };
 
-class SevenOpMock: public Base {
-    public:
-        SevenOpMock() { }
-
-        virtual double evaluate() { return 7.5; }
-        virtual std:: string stringify() { return "7.5"; }
-};
-
-class ZeroOpMock: public Base {
-    public:
-        ZeroOpMock() { }
-
-        virtual double evaluate() { return 0; }
-        virtual std:: string stringify() { return "0"; }
-};
-
-class NegativeOpMock: public Base {
-    public:
-        NegativeOpMock() { }
-
-        virtual double evaluate() { return -1; }
-        virtual std::string stringify() { return "-1"; }
-};
-
-
 #endif //__OP_HPP__
